e901e7bd112c35403d0ce2e3e01fa36da1f3a798
PezEjecutivo <waterlolbusiness@hotmail.com>
2023-11-08 19:12:11 +0100

    Añado css, js e imagenes. Tambien el footer completo, con imagenes y botones funcionales. ademas de tener la paleta de colores ya escogida

---
+++ script.js
@@ -0,0 +1,10 @@
+function CopyMe(TextToCopy) {
+    var TempText = document.createElement("input");
+    TempText.value = TextToCopy;
+    document.body.appendChild(TempText);
+    TempText.select();
+    
+    document.execCommand("copy");
+    document.body.removeChild(TempText);
+    
+  }
